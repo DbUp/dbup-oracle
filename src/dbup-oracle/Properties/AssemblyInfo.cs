2b5a47e9296aac5ac9c1dbc90aff46f0d77ea332
Robert Wagner <robert.wagner@octopus.com>
2024-01-24 12:01:53 +1000

    Added AssemblyInfo

---
+++ src/dbup-oracle/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4f62ae2f-797b-488d-a71e-f27873167fa1")]
